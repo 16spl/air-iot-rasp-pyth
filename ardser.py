710d87c35e13467fab4e06d44f9b11cd6042831d
Mikroudz <severi.olsbo@gmail.com>
2018-03-21 11:08:06 +0200

    Init

--- ardser.py
+++ ardser.py
@@ -1,15 +1,27 @@
 import serial
 import time
 import requests
-import json, struct
+import json, struct, os, serial.tools.list_ports
 from time import sleep
 
 command = 10
-token = "Bearer Ulswzb9BnQnqzIc8rad2ggtt"
+token_file = os.path.expanduser("~/.rasp-pyth-token")
 api_url = "https://air.kiisu.club/v1/data"
 
+arduino_ports = [
+	p.device
+	for p in serial.tools.list_ports.comports()
+]
+
+if not p.device:
+	raise IOError("Arduino not found!")
+
+
+with open(token_file) as f:
+	token = f.readlines()
+
 ser = serial.Serial(
-	port = '/dev/ttyACM0',
+	port = p.device,
 	baudrate = 9600,
 	parity = serial.PARITY_NONE,
 	timeout = 1
@@ -17,8 +29,12 @@ ser = serial.Serial(
 
 def sendData(data):
 	try:
-		send_data = json.dumps({"datum": {"device_id": data['Device_ID'], "temperature": data['Temperature'], "humidity": data['Humidity']}})
-		header = {'Authorization':token,'Content-type': 'application/json'}
+		send_data = json.dumps({"datum": {
+								"device_id": data['Device_ID'],
+								"temperature": data['Temperature'],
+								"humidity": data['Humidity']
+								}})
+		header = {'Authorization':token[0].rstrip(),'Content-type': 'application/json'}
 		req = requests.post(api_url, data=send_data, headers=header)
 	except requests.exceptions.RequestException as e:
 		return e
@@ -26,8 +42,6 @@ def sendData(data):
 		return "Data sent " + str(req.status_code)
 	else:
 		return "Error: " + str(req.status_code)
-
-
 
 ser.flushInput()
 
